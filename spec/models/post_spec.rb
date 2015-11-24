e66ada30af2fdf6ec367d8509d04c2d2202ef956
Troy Harris <tharris@yammer-inc.com>
2015-11-24 20:28:58 +0000

    add spec to check we can store a post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Post, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Post do
+  before do
+    @post = Post.new
+    @post.save
+  end
+
+  it "should contain one post" do
+    post = Post.find 1
+    puts post.id
+  end
 end
